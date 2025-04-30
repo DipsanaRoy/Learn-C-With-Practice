b3bf645602d81f9983706cfa90ab383c7463d1ed
Dipsana Roy <dipsanaroy@gmail.com>
2025-04-30 14:47:58 +0530

    Add C001_Test_Set: Questions & Solutions
    Base Programs

---
+++ C001_Test_Set/cp1_rectangle.c
@@ -0,0 +1,8 @@
+// CP1. Calculate Area of rectangle
+#include<stdio.h>
+
+int main(){
+    int length = 9, breadth = 4;
+    printf("Area of the rectangle is %d", length * breadth);
+    return 0;
+}
